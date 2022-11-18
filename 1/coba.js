2dbef682b8fe426a2c09f17294c26cd15d043618
Lustiyana <lustiyana1801@gmail.com>
2022-11-18 12:08:55 +0700

    first commit

---
+++ 1/coba.js
@@ -0,0 +1,31 @@
+// const getUserSync = (id) => {
+//   const nama = id === 1 ? "Lusti" : "Yana";
+//   return { id, nama };
+// };
+
+// const userSatu = getUserSync(1);
+// console.log(userSatu);
+
+// const userDua = getUserSync(2);
+// console.log(userDua);
+
+// const halo = "Hello World";
+// console.log(halo);
+
+const getUser = (id, cb) => {
+  const time = id === 1 ? 3000 : 2000;
+  setTimeout(() => {
+    const nama = id === 1 ? "Lusti" : "Yana";
+    cb({ id, nama });
+  }, time);
+};
+
+const userSatu = getUser(1, (hasil) => {
+  console.log(hasil);
+});
+const userDua = getUser(2, (hasil) => {
+  console.log(hasil);
+});
+
+const halo = "Hello World";
+console.log("selesai");
